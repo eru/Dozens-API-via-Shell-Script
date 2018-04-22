878de3b41a6fa484a792069205dc9d56b5b2d865
Kazuki Hashimoto <eru.tndl@gmail.com>
2018-04-22 10:01:09 +0900

    mod

--- dynamicIPUpdate.sh
+++ dynamicIPUpdate.sh
@@ -5,27 +5,43 @@ USERNAME="username"
 XAUTHKEY="authkey"
 DOMAINNAME="domainname"
 SERVERNAME="servername"
+DEVICE="eth0"
 
-echo "Dozens dynamic IP setter version 0.3b"
+echo "Dozens dynamic IP setter version 0.3b mod"
 
 # Get Global IP address
-GLOBALIP=$(curl -s ifconfig.me)
-echo "Your global IP is $GLOBALIP"$'\n'
+GLOBALIPV4=$(curl --interface $DEVICE -s http://v4.ipv6-test.com/api/myip.php)
+echo "Your global IPv4 is $GLOBALIPV4"
+GLOBALIPV6=$(curl --interface $DEVICE -s http://v6.ipv6-test.com/api/myip.php)
+echo "Your global IPv6 is $GLOBALIPV6"$'\n'
 
-# auth initialization
-echo "auth initialization start."
-MYKEY=$(curl -s http://dozens.jp/api/authorize.json -H X-Auth-User:$USERNAME -H X-Auth-Key:$XAUTHKEY |sed 's/[{"}:]//g' | sed 's/auth_token//')
+# Auth initialization
+echo "Auth initialization start."
+MYKEY=$(curl -s http://dozens.jp/api/authorize.json -H X-Auth-User:$USERNAME -H X-Auth-Key:$XAUTHKEY | jq -r '.auth_token')
 echo "Your key is : $MYKEY"$'\n'
 
-# Get wikiserver id
-echo "Serching $SERVERNAME record..."
-LINENUMBER=$(echo $(expr $(curl -s http://dozens.jp/api/record/$DOMAINNAME.json -H X-Auth-Token:$MYKEY | tr "," "\n" | grep -n $SERVERNAME | sed 's/[a-z":.]//g') - 1)"p")
+# Get domain records
+echo "Getting $SERVERNAME records..."$'\n'
+JSON=$(curl -s http://dozens.jp/api/record/$DOMAINNAME.json -H X-Auth-Token:$MYKEY)
 
-RECORDID=$(curl -s http://dozens.jp/api/record/$DOMAINNAME.json -H X-Auth-Token:$MYKEY | tr "," "\n" | sed -n $LINENUMBER | sed -e 's/[a-z,",{,},:,.,\[]//g')
-echo "$SERVERNAME record id is $RECORDID""."$'\n'
+# Get server a record id
+echo "Serching $SERVERNAME A record..."
+RECORDID=$(echo $JSON | jq -r '.record[] | select(.name == "$SERVERNAME") | select(.type == "A") | .id')
+echo "$SERVERNAME A record id is $RECORDID""."$'\n'
 
 # Set IP Address
 echo "Recode update in progress..."
-RESULT=$(curl -s -d "{\"prio\":\"\", \"content\":\"$GLOBALIP\", \"ttl\":\"7200\"}" http://dozens.jp/api/record/update/$RECORDID.json -H X-Auth-Token:$MYKEY -H "Host: dozens.jp" -H "Content-Type:application/json" | sed -e 's/\[/\[ /g' -e 's/\]/ \]/g' -e 's/},{/} {/g' | tr " " "\n" | grep $RECORDID)
+RESULT=$(curl -s -d "{\"prio\":\"\", \"content\":\"$GLOBALIPV4\", \"ttl\":\"7200\"}" http://dozens.jp/api/record/update/$RECORDID.json -H X-Auth-Token:$MYKEY -H "Host: dozens.jp" -H "Content-Type:application/json" | jq -r ".record[] | select(.id == \"$RECORDID\")")
 echo "Dozens server says : $RESULT"$'\n'
+
+# Get server aaaa record id
+echo "Serching $SERVERNAME AAAA record..."
+RECORDID=$(echo $JSON | jq -r '.record[] | select(.name == "$SERVERNAME") | select(.type == "AAAA") | .id')
+echo "$SERVERNAME AAAA record id is $RECORDID""."$'\n'
+
+# Set IP Address
+echo "Recode update in progress..."
+RESULT=$(curl -s -d "{\"prio\":\"\", \"content\":\"$GLOBALIPV6\", \"ttl\":\"7200\"}" http://dozens.jp/api/record/update/$RECORDID.json -H X-Auth-Token:$MYKEY -H "Host: dozens.jp" -H "Content-Type:application/json" | jq -r ".record[] | select(.id == \"$RECORDID\")")
+echo "Dozens server says : $RESULT"$'\n'
+
 echo "Script is done!"$'\n'
--- readme.txt
+++ readme.txt
@@ -4,7 +4,14 @@ First, open dynamicIPUpdate.sh with editor application and replace below:
 
 line 4 replace your Dozens user name into username (e.g. johnappleseed).
 line 5 replace your API Key into authkey (e.g. 123abc456def789ghi).
-line 6 replace your server name into domainname (e.g. example.com).
+line 6 replace your domain name into domainname (e.g. example.com).
 line 7 replace your server name into servername (e.g. www.example.com).
+line 8 replace your ethernet device into device (e.g. eth0).
 
 Next, give executable mode to dynamicIPUpdate.sh and running script.
+
+本家との違い
+・グローバルIPアドレスを取得しに行くデバイスを設定するようにしました。
+・DOMAINNAME = SERVERNAME の時に更新できないのを修正しました。
+
+Thanks mutuki !!
