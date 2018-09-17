dee13b524066128fa225158779ecbb110b82221d
Kazuki Hashimoto <eru.tndl@gmail.com>
2018-09-17 21:01:58 +0900

    :rose: :pencil: change variable name

--- README.md
+++ README.md
@@ -2,11 +2,11 @@ dynamicIPUpdate.sh is works for Dozens DNS Record Global IP Addtess update.
 
 First, open dynamicIPUpdate.sh with editor application and replace below:
 
-line 4 replace your Dozens user name into username (e.g. johnappleseed).
-line 5 replace your API Key into authkey (e.g. 123abc456def789ghi).
-line 6 replace your domain name into domainname (e.g. example.com).
-line 7 replace your server name into servername (e.g. www.example.com).
-line 8 replace your ethernet device into device (e.g. eth0).
+line 4 replace your Dozens user name into X\_AUTH\_USER (e.g. johnappleseed).
+line 5 replace your API Key into X\_AUTH\_KEY (e.g. 123abc456def789ghi).
+line 6 replace your zone name into ZONE (e.g. example.com).
+line 7 replace your domain name into DOMAIN (e.g. www.example.com).
+line 8 replace your ethernet device into DEVICE (e.g. eth0).
 
 Next, give executable mode to dynamicIPUpdate.sh and running script.
 
--- dynamicIPUpdate.sh
+++ dynamicIPUpdate.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # KEYWORDS
-USERNAME="username"
-XAUTHKEY="authkey"
-DOMAINNAME="domainname"
-SERVERNAME="servername"
+X_AUTH_USER="user"
+X_AUTH_KEY="key"
+ZONE="zone name"
+DOMAIN="domain name"
 DEVICE="eth0"
 
 echo "Dozens dynamic IP setter version 0.3b mod"
@@ -17,31 +17,31 @@ echo "Your global IPv6 is $GLOBALIPV6"$'\n'
 
 # Auth initialization
 echo "Auth initialization start."
-MYKEY=$(curl -s http://dozens.jp/api/authorize.json -H X-Auth-User:$USERNAME -H X-Auth-Key:$XAUTHKEY | jq -r '.auth_token')
+MYKEY=$(curl -s http://dozens.jp/api/authorize.json -H X-Auth-User:$X_AUTH_USER -H X-Auth-Key:$X_AUTH_KEY | jq -r '.auth_token')
 echo "Your key is : $MYKEY"$'\n'
 
 # Get domain records
-echo "Getting $SERVERNAME records..."$'\n'
-JSON=$(curl -s http://dozens.jp/api/record/$DOMAINNAME.json -H X-Auth-Token:$MYKEY)
+echo "Getting $ZONE records..."$'\n'
+JSON=$(curl -s http://dozens.jp/api/record/$ZONE.json -H X-Auth-Token:$MYKEY)
 
 # Get server a record id
-echo "Serching $SERVERNAME A record..."
-RECORDID=$(echo $JSON | jq -r '.record[] | select(.name == "$SERVERNAME") | select(.type == "A") | .id')
-echo "$SERVERNAME A record id is $RECORDID""."$'\n'
+echo "Serching $DOMAIN A record..."
+RECORD_ID=$(echo $JSON | jq -r '.record[] | select(.name == "$DOMAIN") | select(.type == "A") | .id')
+echo "$DOMAIN A record id is $RECORD_ID""."$'\n'
 
 # Set IP Address
 echo "Recode update in progress..."
-RESULT=$(curl -s -d "{\"prio\":\"\", \"content\":\"$GLOBALIPV4\", \"ttl\":\"7200\"}" http://dozens.jp/api/record/update/$RECORDID.json -H X-Auth-Token:$MYKEY -H "Host: dozens.jp" -H "Content-Type:application/json" | jq -r ".record[] | select(.id == \"$RECORDID\")")
+RESULT=$(curl -s -d "{\"prio\":\"\", \"content\":\"$GLOBALIPV4\", \"ttl\":\"7200\"}" http://dozens.jp/api/record/update/$RECORD_ID.json -H X-Auth-Token:$MYKEY -H "Host: dozens.jp" -H "Content-Type:application/json" | jq -r ".record[] | select(.id == \"$RECORD_ID\")")
 echo "Dozens server says : $RESULT"$'\n'
 
 # Get server aaaa record id
-echo "Serching $SERVERNAME AAAA record..."
-RECORDID=$(echo $JSON | jq -r '.record[] | select(.name == "$SERVERNAME") | select(.type == "AAAA") | .id')
-echo "$SERVERNAME AAAA record id is $RECORDID""."$'\n'
+echo "Serching $DOMAIN AAAA record..."
+RECORD_ID=$(echo $JSON | jq -r '.record[] | select(.name == "$DOMAIN") | select(.type == "AAAA") | .id')
+echo "$DOMAIN AAAA record id is $RECORD_ID""."$'\n'
 
 # Set IP Address
 echo "Recode update in progress..."
-RESULT=$(curl -s -d "{\"prio\":\"\", \"content\":\"$GLOBALIPV6\", \"ttl\":\"7200\"}" http://dozens.jp/api/record/update/$RECORDID.json -H X-Auth-Token:$MYKEY -H "Host: dozens.jp" -H "Content-Type:application/json" | jq -r ".record[] | select(.id == \"$RECORDID\")")
+RESULT=$(curl -s -d "{\"prio\":\"\", \"content\":\"$GLOBALIPV6\", \"ttl\":\"7200\"}" http://dozens.jp/api/record/update/$RECORD_ID.json -H X-Auth-Token:$MYKEY -H "Host: dozens.jp" -H "Content-Type:application/json" | jq -r ".record[] | select(.id == \"$RECORD_ID\")")
 echo "Dozens server says : $RESULT"$'\n'
 
 echo "Script is done!"$'\n'
